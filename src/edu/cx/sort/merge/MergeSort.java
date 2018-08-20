2eca0b9a359643510018ee6c1ff8c2cbba03ec40
caoxiang <xiang.cao@teradata.com>
2018-08-20 15:54:08 +0800

    栈溢出，估计有死循环

--- src/edu/cx/sort/merge/MergeSort.java
+++ src/edu/cx/sort/merge/MergeSort.java
@@ -1,43 +1,80 @@
 package edu.cx.sort.merge;
 
+import edu.cx.sort.SortUtil;
+
 /**
- * Created by caoxiang on 2018/8/14.
+ * 将两个有序数组归并为一个有序数组
+ *  栈溢出，估计有死循环
  */
 public class MergeSort {
+    private static Comparable[] aux;//原地归并
 
-    public static void merge(Comparable[] a, int lo, int mid, int hi) {
-        Comparable[] aux = new Comparable[a.length];
-        //合并 lo~mid  和 mid+1 ~ hi
-        int i = lo;
-        int j = mid + 1;
-
-        for (int k = lo; k < hi; k++) {
-            aux[k] = a[k];
-        }
-
-        for (int k = lo; k < hi; k++) {
-            if (i > mid) {
-                a[k] = aux[j++];
-            } else if (j>hi) {
-                a[k] = aux[i++];
-            } else if (j>hi) {
-                a[k] = aux[j++];
-            }else {
-                a[k] = aux[i++];
-            }
-        }
-
-        for (Comparable c : a) {
-            System.out.println((Integer) c);
-
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+        for (Comparable num : a) {
+            System.out.print(num + "->");
         }
 
 
     }
 
+    private static void sort(Comparable[] a, int lo, int hi) {
+
+        if (hi <= lo) {
+            return;//递归  第一规律
+        }
+
+        int mid = lo + (lo + hi) / 2;
+        sort(a, lo, mid);//左半边归并
+        sort(a, mid + 1, hi);//右半边归并
+        merge(a, lo, mid, hi);
+
+    }
+
+
+    public static Comparable[] merge(Comparable[] a, int lo, int mid, int hi) {
+//        Comparable[] aux = new Comparable[a.length];
+        //合并 lo~mid  和 mid+1 ~ hi
+        int i = lo;
+        int j = mid + 1;//前后分割点
+
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {//左半边用尽，取右半边元素
+                a[k] = aux[j++];
+            } else if (j > hi) {//右半边用尽,取左半边元素
+                a[k] = aux[i++];
+            } else if (SortUtil.less(aux[j],aux[i])) {//右半边当前元素小于左半边当前元素，取小的。
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];//左半边当前元素 < 右半边  取小。
+            }
+        }
+
+        return a;
+
+
+    }
+
     public static void main(String[] args) {
-        Integer[] a = new Integer[]{2, 3,1};
-        merge(a, 0, 1, 1);
+       /* Integer[] a = new Integer[]{ 3, 1};
+        int lo = 0;
+        int hi = a.length -1;
+        int mid = lo+(hi-lo)/2;
+        Comparable[] merge = merge(a, lo, mid, hi);
+        for (Comparable comparable : merge) {
+            System.out.print(comparable+"->");
+
+        }*/
+
+        Integer[] a = new Integer[]{4,2,3, 1};
+
+        sort(a);
+
 
     }
 }
