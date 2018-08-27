95503d55106d4c3437c82160fc00acf93a285c49
caoxiang <xiang.cao@teradata.com>
2018-08-27 21:52:18 +0800

    SequentialSearchST ok

--- src/edu/cx/searching/balanceTree/SequentialSearchST.java
+++ src/edu/cx/searching/balanceTree/SequentialSearchST.java
@@ -4,7 +4,7 @@ package edu.cx.searching.balanceTree;
  * 基于无序链表
  */
 public class SequentialSearchST<Key, Value> {
-    private Node first;
+    private Node first;//首节点
 
 
     private class Node {
@@ -21,14 +21,35 @@ public class SequentialSearchST<Key, Value> {
     }
 
     public Value get(Key key){
+        for(Node x = first;x!=null;x = x.next){
+            if (x.key.equals(key)) {
+                return x.val;
+            }
+        }
+
         return null;
     }
 
     public void  put(Key key,Value value){
+        for(Node x = first;x!=null;x = x.next){
+            if (key.equals(x.key)) {
+                x.val = value;
+            }
+
+        }
+
+        first = new Node(key, value, first);//插入链表头
+
 
     }
 
     public static void main(String[] args) {
+
+        SequentialSearchST st = new SequentialSearchST();
+        st.put("1","dog");
+        st.put("2","cat");
+        st.put("1","fox");
+
 
     }
 
