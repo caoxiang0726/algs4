c5d62f267c122681a70c3061b0b4a2c08853e0c6
cao x <1044434566@qq.com>
2020-01-19 14:18:22 +0800

    二叉树遍历

--- src/edu/cx/searching/bst/BinaryTreeTest.java
+++ src/edu/cx/searching/bst/BinaryTreeTest.java
@@ -1,122 +1,93 @@
 package edu.cx.searching.bst;
 
-/*
- * 讲解：
- * 二个方法函数，一个寻找关键字--searchkey 另一个是插入一个结点：insertTree
- * 另外这是一个完全的先序遍历二叉树的语法。先根结点，再左结点，如无再右结点，
- * 如此递归至搜索完毕。
- *
- */
+
 public class BinaryTreeTest {
-	private static BinaryTree root = null;
+    private static Node rootNode = null;
 
-	public BinaryTreeTest() {
-		init();
-	}
+    public BinaryTreeTest() {
+        init();
+    }
 
-	/**
-	 * 初始化给定数据的二叉树结构
-	 * 
-	 */
-	private void init() {
-		int data[] = { 12, 11, 34, 45, 67, 38, 56, 43, 22, 8 };
-		root = new BinaryTree(data[0]);
-		System.out.println("构造的二叉树的数据结构:");
-		System.out.println("--------------start----------------------");
-		System.out.println(data[0] + ":root");
-		for (int i = 1; i < data.length; i++) {
-			System.out.print(data[i] + ":");
-			root.insertTree(root, data[i]);
-		}
-		System.out.println("----------------end--------------------");
-	}
+    private void init() {
+        int data[] = {12, 11, 34, 45, 67, 38, 56, 43, 22, 8};
+        rootNode = new Node(data[0]);
+        System.out.println("构造的二叉树的数据结构:");
+        System.out.println("--------------start----------------------");
+        System.out.println(data[0] + ":rootNode");
+        for (int i = 1; i < data.length; i++) {
+            System.out.print(data[i] + ":");
+            rootNode.insertTree(rootNode, data[i]);
+        }
+        System.out.println("----------------end--------------------");
+    }
 
-	/*
-	 * public void serach(int key) { if (searchkey(root, key)) {
-	 * System.out.println("找到了:" + key); } else { System.out.println("没有找到：" +
-	 * key); } }
-	 * 
-	 * private boolean searchkey(BinaryTree root, int key) { if (root == null) {
-	 * return false; } else if (root.data == key) { return true; } else if (key
-	 * >= root.data) { return searchkey(root.rightpoiter, key); } return
-	 * searchkey(root.leftpoiter, key); }
-	 */
+    class Node {
+        int data;//数据域
+        Node leftChild;//指针域
+        Node rightChild;
 
-	class BinaryTree {
-		int data;
-		BinaryTree lchild;
-		BinaryTree rchild;
+        Node(int data) {
+            this.data = data;
+            leftChild = null;// 构造一个新节点是，左右子树为空
+            rightChild = null;
+        }
 
-		BinaryTree(int data) {
-			this.data = data;
-			lchild = null;// 构造一个新节点是，左右子树为空
-			rchild = null;
-		}
+        private void insertTree(Node root, int data) {
+            /**
+             * 1 对于新节点还没递归而言，每次的root都是从开始的。树的节点越多，比如30层，每新增一个节点都要遍历30次。
+             * 2 递归中的，有些节点临时充当根节点。
+             */
+            if (data >= root.data) {
+                if (root.rightChild == null) {
+                    System.out.println(" -> new rightChild");
+                    root.rightChild = new Node(data);
+                } else {
+                    System.out.print(" -> rightChild");
+                    // 递归
+                    insertTree(root.rightChild, data);
+                }
+            } else {
+                if (root.leftChild == null) {
+                    System.out.println(" -> new leftChild");
+                    root.leftChild = new Node(data);
+                } else {
+                    System.out.print(" -> leftChild");
+                    insertTree(root.leftChild, data);
+                }
+            }
+        }
+    }
 
-		private void insertTree(BinaryTree root, int data) {
-			/**
-			 * 1 对于新节点还没递归而言，每次的root都是从12开始的。树的节点越多，比如30层，每新增一个节点都要遍历30次。 2
-			 * 递归中的，有些节点临时充当根节点。
-			 */
-			if (data >= root.data) {
-				if (root.rchild == null) {
-					System.out.println(" -> new rchild");
-					root.rchild = new BinaryTree(data);
-				} else {
-					System.out.print(" -> rchild");
-					// 递归
-					insertTree(root.rchild, data);
-				}
-			} else {
-				if (root.lchild == null) {
-					System.out.println(" -> new lchild");
-					root.lchild = new BinaryTree(data);
-				} else {
-					System.out.print(" -> lchild");
-					insertTree(root.lchild, data);
-				}
-			}
-		}
-	}
 
-	public static void preTraverse(BinaryTree root) {
-		if (root != null) {
-			System.out.print(root.data + " ");
-			preTraverse(root.lchild);
-			preTraverse(root.rchild);
-		}
-	}
+    public static void main(String args[]) {
+        // 创建一棵树
+        BinaryTreeTest b = new BinaryTreeTest();
+        // Node rootNode = new Node(12);
+        System.out.println("前序遍历----");
+        preOrderTraverse(rootNode);
+    }
 
-	/**
-	 * 中序遍历
-	 */
-	public static void inTraverse(BinaryTree root) {
-		if (root != null) {
-			inTraverse(root.lchild);
-			System.out.print(root.data + " ");
-			inTraverse(root.rchild);
-		}
-	}
+    public static void preOrderTraverse(Node root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preOrderTraverse(root.leftChild);
+            preOrderTraverse(root.rightChild);
+        }
+    }
 
-	/**
-	 * 后序遍历
-	 */
-	public static void postTraverse(BinaryTree root) {
-		if (root != null) {
-			postTraverse(root.lchild);
-			postTraverse(root.rchild);
-			System.out.print(root.data + " ");
-		}
-	}
+    public static void inOrderTraverse(Node root) {
+        if (root != null) {
+            inOrderTraverse(root.leftChild);
+            System.out.print(root.data + " ");
+            inOrderTraverse(root.rightChild);
+        }
+    }
 
-	public static void main(String args[]) {
-		// 创建一棵树
-		BinaryTreeTest b = new BinaryTreeTest();
-		// BinaryTree root = new BinaryTree(12);
-		System.out.println("前序遍历----");
-		preTraverse(root);
-		//
-		// int key = 8; // key：任意数值
-		// b.serach(key); // 到二叉树中查找
-	}
+    public static void postOrderTraverse(Node root) {
+        if (root != null) {
+            postOrderTraverse(root.leftChild);
+            postOrderTraverse(root.rightChild);
+            System.out.print(root.data + " ");
+        }
+    }
 }
